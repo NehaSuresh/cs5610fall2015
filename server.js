01c682aca6c276e8ac0f25be289758fafe5744a3
nehasuresh02 <danayakapura.n@husky.neu.edu>
2015-11-16 17:09:42 -0800

    Assignment4 bugs

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 require("./public/assignment4/server/app.js")(app);
